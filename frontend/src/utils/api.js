771a1f550326314f5be629b6905e10472bfa2802
Shifa Tazeen Shaikh <shifatazeenshaikh@Mac.lan>
2025-09-16 12:22:34 +0530

    cors updated and paths changed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 
 const api = axios.create({
-baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
+baseURL: "https://nailib-backend-url.onrender.com/api",
+
 headers: { 'Content-Type': 'application/json' },
 })
 
