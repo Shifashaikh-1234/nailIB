d70288be46fb2b123e3613563c5c08c570133874
Shifa Tazeen <shifatazeen.msc@gmail.com>
2025-09-16 13:54:08 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import api from "../utils/api"
-import {useNavigate} from react-router-dom
+
 import { saveToken } from "../utils/auth"
 
 export default function Signup() {
@@ -8,7 +8,7 @@ export default function Signup() {
   const [password, setPassword] = useState("")
   const [msg, setMsg] = useState("")
   const [loading, setLoading] = useState(false)
-   const navigate = useNavigate();
+
 
   async function handleSubmit(e) {
     e.preventDefault()
@@ -17,7 +17,7 @@ export default function Signup() {
       const r = await api.post("/auth/signup", { email, password })
       saveToken(r.data.token)
       setMsg("✅ Signup successful! Token saved.")
-      navigate("/login")
+
       
       
     } catch (err) {
